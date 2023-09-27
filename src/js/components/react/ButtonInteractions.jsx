41503027cfbc447f70e4fdbb080f0485e062302a
ValeriaAcuM <106450335+ValeriaAcuM@users.noreply.github.com>
2023-09-27 00:45:38 -0600

    state interested button

--- src/js/components/react/ButtonInteractions.jsx
+++ src/js/components/react/ButtonInteractions.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-const GoingMessage = ({handleGoingClick}) => {
+const GoingMessage = ({ handleGoingClick }) => {
   return (
     <>
       <span className="check">✔</span>
       <div>
-        <p>You &apos re going to this event!</p>
+        <p>Youre going to this event!</p>
         <button
           data-id="${id}"
           data-interaction="${going}"
@@ -20,47 +20,83 @@ const GoingMessage = ({handleGoingClick}) => {
   );
 };
 
+const InterestedMessage = ({ handleInterestedClick, handleGoingClick}) => {
+  const HandleClick = () => {
+    handleInterestedClick(),
+    handleGoingClick()
+  }
+
+  return (
+    <>
+      <div>
+        <p>Youre interested in going.</p>
+        <button
+          data-id="${id}"
+          data-interaction="${interested}"
+          data-template="${remove}"
+          className="button-link"
+          onClick={handleInterestedClick}
+        >
+          Changed your mind?
+        </button>
+      </div>
+      <button
+        className="going"
+        data-id="${id}"
+        data-interaction="${going}"
+        data-template="${going}"
+        onClick={HandleClick}
+      >
+        Going!
+      </button>
+    </>
+  );
+};
+
 const EventInteractions = () => {
   const [going, setGoing] = useState(false);
+  const [interested, setInterested] = useState(false);
 
   function handleGoingClick() {
     setGoing(!going);
   }
 
-    return (
-      <>
-        {going ? null : (
-          <div className="going-and-interested" data-id="${id}">
-            <button
-              className="going"
-              data-id="${id}"
-              data-interaction="${going}"
-              data-template="${going}"
-              onClick={handleGoingClick}
-            >
-              Going!
-            </button>
-            <button
-              className="interested"
-              data-id="${id}"
-              data-interaction="${interested}"
-              data-template="${interested}"
-            >
-              Interested
-            </button>
-          </div>
-        )}
-        <button
-          className="heart"
-          data-id="${id}"
-          data-interaction="${favorites}"
-        >
-          Favorite
-        </button>
+  function handleInterestedClick() {
+    setInterested(!interested);
+  }
 
-        {going ? <GoingMessage handleGoingClick={handleGoingClick} /> : null}
-      </>
+  return (
+    <>
+      {going || interested ? null : (
+        <div className="going-and-interested" data-id="${id}">
+          <button
+            className="going"
+            data-id="${id}"
+            data-interaction="${going}"
+            data-template="${going}"
+            onClick={handleGoingClick}
+          >
+            Going!
+          </button>
+          <button
+            className="interested"
+            data-id="${id}"
+            data-interaction="${interested}"
+            data-template="${interested}"
+            onClick={handleInterestedClick}
+          >
+            Interested
+          </button>
+        </div>
+      )}
+      <button className="heart" data-id="${id}" data-interaction="${favorites}">
+        Favorite
+      </button>
+      {going ? <GoingMessage handleGoingClick={handleGoingClick} /> : null}
+      {interested ? <InterestedMessage handleInterestedClick={handleInterestedClick} handleGoingClick={handleGoingClick} /> : null}
+      
+    </>
   );
 };
 
-export { EventInteractions }
+export { EventInteractions };
