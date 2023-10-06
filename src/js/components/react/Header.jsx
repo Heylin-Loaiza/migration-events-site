39c315f8c1d80309521d398f39a4a1aaf0d7d9cc
javierbarbozam <barboza.javier497@gmail.com>
2023-10-06 15:14:53 -0600

    Create header component

--- src/js/components/react/Header.jsx
+++ src/js/components/react/Header.jsx
@@ -1,10 +1,32 @@
+const tabsList = () => {
+  const tabs = ["music", "sports", "business", "food", "art"];
+  return (
+    <ul id="tabs" className="header__tabs">
+      {tabs.map((item) => (
+        <li className="header__tabs-item" key={item} data-category={item}>
+          {item}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const accountTab = () => {
+  // add state (Go to Account or Go back to events)
+  return (
+    <a href="./account.html" id="my-account">
+      My account &rarr;
+    </a>
+  );
+};
+
 const Header = () => {
   return (
-    <header id="header" class="header">
-      <a href="./account.html" id="my-account">My account &rightarrow;</a>
-      <div id="tabs" class="header__tabs"></div>
+    <header id="header" className="header">
+      {accountTab()}
+      {tabsList()}
     </header>
-  )
-}
+  );
+};
 
-export { Header }
+export { Header };
