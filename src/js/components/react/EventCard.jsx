ab967e6607ba8a2bb38b13d9903b7616343a5f8d
javierbarbozam <barboza.javier497@gmail.com>
2023-10-06 20:10:18 -0600

    generate static cards

--- src/js/components/react/EventCard.jsx
+++ src/js/components/react/EventCard.jsx
@@ -1,5 +1,30 @@
-const EventCard = (props) => {
-return (<div />);
-}
+const Card = ({ event }) => {
+  const { image, title, date, location, price } = event;
+  return (
+    <li className="gallery__card">
+      <img src={image} alt={title}/>
+      <div className="gallery__text">
+        <div className="event__info">
+          <h3>{title}</h3>
+          <p className="date">{date}.</p>
+          <p>
+            {location.address} • {location.city}, {location.state}.
+          </p>
+          <strong>${price}</strong>
+        </div>
+      </div>
+    </li>
+  );
+};
 
-export {EventCard}
+const EventCard = ({props}) => {
+  return (
+    <ul className="container error-container gallery home-gallery">
+      {props.map((item) => (
+        <Card key={item.id} event={item} />
+      ))}
+    </ul>
+  );
+};
+
+export { EventCard };
